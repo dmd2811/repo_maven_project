4c0c5cfc3d94adeafab36460f77689c15c2a02c3
dmd2811 <domanhdung281100@gmail.com>
2024-09-22 02:57:57 +0700

    file Test: add Test_case

--- src/test/java/my_app/CalculatorTest.java
+++ src/test/java/my_app/CalculatorTest.java
@@ -4,9 +4,33 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 public class CalculatorTest {
 	@Test
-    public void testAdd() {
+    public void testAdd1() {
 		Calculator cal = new Calculator();
         int result = cal.add(2, 3);
+        assertEquals(5, result);
+    }
+	@Test
+    public void testAdd2() {
+		Calculator cal = new Calculator();
+        int result = cal.add(2, 4);
+        assertEquals(5, result);
+    }
+	@Test
+    public void testAdd3() {
+		Calculator cal = new Calculator();
+        int result = cal.add(3, 2);
+        assertEquals(5, result);
+    }
+	@Test
+    public void testAdd4() {
+		Calculator cal = new Calculator();
+        int result = cal.add(4, 4);
+        assertEquals(5, result);
+    }
+	@Test
+    public void testAdd5() {
+		Calculator cal = new Calculator();
+        int result = cal.add(6, 3);
         assertEquals(5, result);
     }
 }
